ff3c7306516434d5dd481400248a626237020e81
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-07-01 22:05:51 +0600

    1723

--- asset/index.js
+++ asset/index.js
@@ -12,3 +12,17 @@ function slideright(){
     document.getElementById('slide1').classList.add('slideanimation')
 
 }
+function baner2imganimation(){
+    document.getElementById('baner2-txt').classList.add('baner-img-animation')
+    
+}
+function baner2imganimationremove(){
+    document.getElementById('baner2-txt').classList.remove('baner-img-animation')
+    
+}
+document.getElementById('app').addEventListener('mouseover',function(){
+    document.getElementById('app-img').classList.add('appanimation')
+})
+document.getElementById('app').addEventListener('mouseout',function(){
+    document.getElementById('app-img').classList.remove('appanimation')
+})
