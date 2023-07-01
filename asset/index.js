3639b7589ac04d1afe2c1959ab40226c328350ef
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-07-01 17:37:08 +0600

    1723

---
+++ asset/index.js
@@ -0,0 +1,14 @@
+function slideleft(){
+    document.getElementById('slide1').style.display="none"
+    
+    document.getElementById('slide2').style.display="flex"
+    document.getElementById('slide2').classList.add('slideanimation')
+
+}
+function slideright(){
+    document.getElementById('slide2').style.display="none"
+    
+    document.getElementById('slide1').style.display="flex"
+    document.getElementById('slide1').classList.add('slideanimation')
+
+}
