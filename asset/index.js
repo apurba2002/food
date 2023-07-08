11a944d32302a5422be858d5098939baf8e519cf
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-07-08 12:55:12 +0600

    7823

--- asset/index.js
+++ asset/index.js
@@ -1,55 +1,15 @@
-function slideleft(){
-    document.getElementById('slide1').style.display="none"
-    
-    document.getElementById('slide2').style.display="flex"
+function slideleft() {
+    document.getElementById('slide1').style.display = "none"
+
+    document.getElementById('slide2').style.display = "flex"
     document.getElementById('slide2').classList.add('slideanimation')
 
 }
-function slideright(){
-    document.getElementById('slide2').style.display="none"
-    
-    document.getElementById('slide1').style.display="flex"
+function slideright() {
+    document.getElementById('slide2').style.display = "none"
+
+    document.getElementById('slide1').style.display = "flex"
     document.getElementById('slide1').classList.add('slideanimation')
 
 }
-function baner2imganimation(){
-    document.getElementById('baner2-txt').classList.add('baner-img-animation')
-    
-}
-function baner2imganimationremove(){
-    document.getElementById('baner2-txt').classList.remove('baner-img-animation')
-    
-}
-document.getElementById('app').addEventListener('mouseover',function(){
-    document.getElementById('app-img').classList.add('appanimation')
-})
-document.getElementById('app').addEventListener('mouseout',function(){
-    document.getElementById('app-img').classList.remove('appanimation')
-})
-document.getElementById('menu').addEventListener('mouseover',function(){
-    document.getElementById('menugrp').classList.add('menuanimation')
-})
-document.getElementById('review').addEventListener('mouseover',function(){
-    document.getElementById('person').classList.add('reviewperson')
-})
-document.getElementById('review').addEventListener('mouseout',function(){
-    document.getElementById('person').classList.remove('reviewperson')
-})
-document.getElementById('review').addEventListener('mouseover',function(){
-    document.getElementById('ri').classList.add('reviewimg')
-})
-document.getElementById('review').addEventListener('mouseout',function(){
-    document.getElementById('ri').classList.remove('reviewimg')
-})
-document.getElementById('review').addEventListener('mouseover',function(){
-    document.getElementById('offer').classList.add('reviewbtn')
-})
-document.getElementById('review').addEventListener('mouseout',function(){
-    document.getElementById('offer').classList.remove('reviewbtn')
-})
-document.getElementById('review').addEventListener('mouseover',function(){
-    document.getElementById('rtxt').classList.add('reviewtxt')
-})
-document.getElementById('review').addEventListener('mouseout',function(){
-    document.getElementById('rtxt').classList.remove('reviewtxt')
-})
+
